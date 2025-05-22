a79fbbbfeb22a5f909bfae08ff7f734280acc1a8
Ryon McAuley <rdmcauley@gmail.com>
2025-05-22 19:13:13 -0400

    add htmx frontend

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -10,6 +12,8 @@ func NewRouter() *mux.Router {
 	v1 := r.PathPrefix("/api/v1").Subrouter()
 	v1.HandleFunc("/v1/hello", handleHello).Methods("GET")
 	r.HandleFunc("/hello", handleHello).Methods("GET")
+
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./web")))
 
 	return r
 }
